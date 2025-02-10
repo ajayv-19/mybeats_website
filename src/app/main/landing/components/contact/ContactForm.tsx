1d5de7ee484cd6f9b4c8aee592628b820139e3db
imtanmay21 <imtanmay2112@gmail.com>
2025-02-10 15:34:39 -0500

    Added contact form handling

--- src/app/main/landing/components/contact/ContactForm.tsx
+++ src/app/main/landing/components/contact/ContactForm.tsx
@@ -1,24 +1,50 @@
 import Button from "app/shared-components/buttons/Button";
 import TextArea from "app/shared-components/inputs/TextArea";
 import TextInput from "app/shared-components/inputs/TextInput";
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 function ContactForm() {
+  const [firstName, setFirstName] = useState<string>();
+  const [lastName, setLastName] = useState<string>();
+  const [email, setEmail] = useState<string>();
+  const [message, setMessage] = useState<string>();
+
+  const handleSubmit = async () => {
+    try {
+      
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="flex flex-col p-20 bg-white shadow-xl rounded-32 space-y-20">
       <div className="flex flex-row space-x-20">
-        <TextInput label="First name" />
-        <TextInput label="Last name" />
+        <TextInput
+          onChange={(e) => setFirstName(e.target.value)}
+          value={firstName}
+          label="First name"
+        />
+        <TextInput
+          onChange={(e) => setLastName(e.target.value)}
+          value={lastName}
+          label="Last name"
+        />
       </div>
-      <TextInput label="Email" />
-      <TextArea />
+      <TextInput
+        label="Email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+      />
+      <TextArea value={message} onChange={(e) => setMessage(e.target.value)} />
       <div className="flex flex-col items-center justify-center">
-        <Button variant="contained" onClick={() => {}}>
+        <Button variant="contained" onClick={() => handleSubmit()}>
           <span className="text-white">Submit</span>
         </Button>
       </div>
     </div>
   );
-};
+}
 
 export default ContactForm;
