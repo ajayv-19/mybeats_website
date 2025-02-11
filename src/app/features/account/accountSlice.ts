c28edbaefcd9535b3682404f4401b02fc0d55120
imtanmay21 <imtanmay2112@gmail.com>
2025-02-11 12:28:37 -0500

    Fixed account setup error

--- src/app/features/account/accountSlice.ts
+++ src/app/features/account/accountSlice.ts
@@ -64,64 +64,52 @@ export const submitAccountDetails = createAsyncThunk(
       profileImageLink: File | null;
       defaultEmail: string;
     },
-    { rejectWithValue },
-  ) =>
-    async (
-      {
-        formData,
-        profileImageLink,
-        defaultEmail,
-      }: {
-        formData: UserDetails;
-        profileImageLink: File | null;
-        defaultEmail: string;
-      },
-      { rejectWithValue },
-    ) => {
-      try {
-        let linkFromS3 = "";
+    { rejectWithValue }
+  ) => {
+    try {
+      let linkFromS3 = "";
+
+      // Upload the image to S3 if a profile image link exists
+      if (profileImageLink) {
+        const sanitizedEmail = defaultEmail.replace(/[.@]/g, ""); // Sanitize email
+        const fileExtension = profileImageLink.name.substring(
+          profileImageLink.name.lastIndexOf(".")
+        );
+        const fileName = `profiles/${sanitizedEmail}${fileExtension}`;
 
         // Upload the image to S3 if a profile image link exists
         if (profileImageLink) {
           const sanitizedEmail = defaultEmail.replace(/[.@]/g, ""); // Sanitize email
           const fileExtension = profileImageLink.name.substring(
-            profileImageLink.name.lastIndexOf("."),
+            profileImageLink.name.lastIndexOf(".")
           );
           const fileName = `profiles/${sanitizedEmail}${fileExtension}`;
 
-          // Upload the image to S3 if a profile image link exists
-          if (profileImageLink) {
-            const sanitizedEmail = defaultEmail.replace(/[.@]/g, ""); // Sanitize email
-            const fileExtension = profileImageLink.name.substring(
-              profileImageLink.name.lastIndexOf("."),
-            );
-            const fileName = `profiles/${sanitizedEmail}${fileExtension}`;
+          // Upload image
+          // TODO: uploadData is deprecated, have to change this method.
+          const result = await uploadData({
+            key: fileName,
+            data: profileImageLink,
+          }).result;
 
-            // Upload image
-            // TODO: uploadData is deprecated, have to change this method.
-            const result = await uploadData({
-              key: fileName,
-              data: profileImageLink,
-            }).result;
-
-            linkFromS3 = `https://insurance-dashboard-imagesdd445-dev.s3.us-east-1.amazonaws.com/public/${result.key}`;
-            formData = { ...formData, image: linkFromS3 };
-          }
+          linkFromS3 = `https://insurance-dashboard-imagesdd445-dev.s3.us-east-1.amazonaws.com/public/${result.key}`;
+          formData = { ...formData, image: linkFromS3 };
         }
-
-        // Submit the account details to the API
-        const response = await addOrUpdateUser(formData);
-
-        if (response.status === 200) {
-          return response.data.userdata as UserDetails;
-        }
-
-        return rejectWithValue("Failed to submit account details");
-      } catch (error) {
-        console.error("Failed to update account settings:", error);
-        return rejectWithValue(error.message || "Unknown error");
       }
-    },
+
+      // Submit the account details to the API
+      const response = await addOrUpdateUser(formData);
+
+      if (response.status === 200) {
+        return response.data.userdata as UserDetails;
+      }
+
+      return rejectWithValue("Failed to submit account details");
+    } catch (error) {
+      console.error("Failed to update account settings:", error);
+      return rejectWithValue(error.message || "Unknown error");
+    }
+  }
 );
 
 /**
@@ -131,7 +119,7 @@ export const submitCompanyDetails = createAsyncThunk(
   "account/submitCompany",
   async (
     { formData }: { formData: CompanyFormInput },
-    { getState, dispatch, rejectWithValue },
+    { getState, dispatch, rejectWithValue }
   ) => {
     try {
       const state = getState() as RootState; // Ensure correct typing
@@ -159,7 +147,7 @@ export const submitCompanyDetails = createAsyncThunk(
             Authorization: authToken,
             "Content-Type": "application/json",
           },
-        },
+        }
       );
 
       dispatch(fetchAccountDetails());
@@ -169,7 +157,7 @@ export const submitCompanyDetails = createAsyncThunk(
       console.error("error", error);
       return rejectWithValue(error.response?.data?.message || "Unknown error");
     }
-  },
+  }
 );
 
 /**
@@ -194,7 +182,7 @@ export const fetchAccountDetails = createAsyncThunk(
           headers: {
             Authorization: authToken,
           },
-        },
+        }
       );
 
       if (response.status === 200) {
@@ -206,7 +194,7 @@ export const fetchAccountDetails = createAsyncThunk(
     } catch (error) {
       return rejectWithValue(error.message || "Unknown error");
     }
-  },
+  }
 );
 
 /**
