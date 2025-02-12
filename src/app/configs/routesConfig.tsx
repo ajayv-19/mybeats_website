63dd7c467911ec08aaf1d48a54c80e1109f05872
imtanmay21 <imtanmay2112@gmail.com>
2025-02-11 21:08:59 -0500

    navigating to account when signing up

--- src/app/configs/routesConfig.tsx
+++ src/app/configs/routesConfig.tsx
@@ -36,7 +36,7 @@ const routes: FuseRoutesType = [
     children: [
       {
         path: "/",
-        element: <Navigate to="/apps/profile" />,
+        element: <Navigate to="/apps/settings/account" />,
       },
       ...mainRoutes,
       {
