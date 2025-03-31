f22839119bf78ae23657739646d4e251e8b8c22c
imtanmay21 <imtanmay2112@gmail.com>
2025-03-31 19:36:02 -0400

    Created the pilot page

---
+++ src/app/main/consentForm/ConsentFormPage.tsx
@@ -0,0 +1,301 @@
+import React from "react";
+import Navbar from "../landing/components/navbar/Navbar";
+import Footer from "../landing/components/footer/Footer";
+
+import AppleStoreBtn from "../../../assets/download_app_store.svg";
+import GooglePlayBtn from "../../../assets/googleplay.png";
+
+const ConsentFormPage = () => {
+  return (
+    <div className="bg-gray-200 overflow-x-hidden">
+      <Navbar />
+      <div className="pb-[100px] overflow-auto flex flex-col items-center justify-center">
+        <div className="px-40 md:w-[1000px] flex flex-col gap-20">
+          <div className="py-6">
+            <p>LAST UPDATED: March 18, 2025</p>
+
+            <p>
+              You are invited to participate in a pilot study aimed at
+              evaluating an AI-based health monitoring application designed to
+              improve firefighter health and provide advanced insurance
+              analytics. We are conducting this pilot test in collaboration with
+              your insurance company.
+            </p>
+
+            <h2 className="mt-20 font-bold text-xl mb-2">
+              1. Invitation &amp; Voluntary Participation
+            </h2>
+            <ul className="list-disc list-inside space-y-2">
+              <li>Participation is completely voluntary.</li>
+              <li>
+                Your decision to participate, withdraw, or not participate will
+                not affect your relationship with any entity.
+              </li>
+              <li>
+                You may withdraw at any time without penalty by using the
+                “Withdraw Participation” option in the app or by uninstalling
+                the app.
+              </li>
+              <li>
+                Withdrawal does not affect the use of data collected previously
+                during your participation.
+              </li>
+            </ul>
+
+            <h2 className="mt-20 font-bold text-xl mb-2">
+              2. Purpose of the Study
+            </h2>
+            <ul className="list-disc list-inside space-y-2">
+              <li>
+                Assess the effectiveness of an AI-powered mobile application in
+                providing personalized health insights to firefighters and
+                enabling them to improve their health.
+              </li>
+              <li>
+                Use wearable health tracker data to create analytics for
+                insurance companies.
+              </li>
+            </ul>
+
+            <h2 className="mt-20 font-bold text-xl mb-2">3. Eligibility</h2>
+            <ul className="list-disc list-inside space-y-2">
+              <li>You are an active firefighter.</li>
+              <li>
+                You agree to wear a health tracker (either one provided by the
+                study or your own device).
+              </li>
+              <li>
+                You are willing to sync your device with our mobile app for data
+                collection over a 30-day period.
+              </li>
+            </ul>
+
+            <h2 className="mt-20 font-bold text-xl mb-2">
+              4. Study Procedures
+            </h2>
+            <p>
+              If you agree to participate, you will be asked to do the following
+              over a 30-day period:
+            </p>
+
+            <h3 className="font-bold text-lg mb-2">Health Tracker Use:</h3>
+            <ul className="list-disc list-inside space-y-2">
+              <li>You will receive a health tracker or use your own.</li>
+              <li>
+                You are expected to charge and wear the tracker as consistently
+                as possible.
+              </li>
+            </ul>
+
+            <h3 className="font-bold text-lg mb-2">Data Syncing:</h3>
+            <ul className="list-disc list-inside space-y-2">
+              <li>
+                Connect and sync your health tracker with our mobile app daily.
+              </li>
+              <li>
+                The app will automatically upload your data (e.g., heart rate,
+                activity levels, sleep patterns, caloric intake, etc.) directly
+                to our AI models without manual intervention.
+              </li>
+            </ul>
+
+            <h3 className="font-bold text-lg mb-2">AI-Health Insights:</h3>
+            <ul className="list-disc list-inside space-y-2">
+              <li>
+                Receive personalized health insights and recommendations within
+                the app.
+              </li>
+              <li>
+                These insights are for informational purposes only and do not
+                constitute medical advice.
+              </li>
+              <li>
+                Generate points based on wearables data and weekly leaderboards
+                to encourage community engagement and promote a healthy
+                lifestyle among participants.
+              </li>
+              <li>
+                The gamification and social features are provided solely for
+                motivational purposes and are not intended for performance
+                evaluation.
+              </li>
+            </ul>
+
+            <h3 className="font-bold text-lg mb-2">Feedback Submission:</h3>
+            <ul className="list-disc list-inside space-y-2">
+              <li>
+                At the end of the pilot study, you will be asked to complete a
+                brief survey regarding your experience with the app.
+              </li>
+              <li>
+                Survey responses will be de-linked from personally identifiable
+                information and used only for aggregate analysis.
+              </li>
+              <li>
+                After the pilot test, you may disconnect the health tracker from
+                the app and return to normal device usage.
+              </li>
+              <li>
+                If provided with a health tracker, please return it. Contact us
+                at contact@mybeatshealth.com for return instructions.
+              </li>
+            </ul>
+
+            <h2 className="mt-20 font-bold text-xl mb-2">
+              5. Data Collection, Use &amp; Privacy
+            </h2>
+
+            <h3 className="font-bold text-lg mb-2">What We Collect:</h3>
+            <ul className="list-disc list-inside space-y-2">
+              <li>
+                Health data (heart rate, activity, sleep patterns, caloric
+                intake, etc.) from your wearable device.
+              </li>
+              <li>
+                App interaction data (usage logs, engagement with gamifications
+                and social features, survey responses).
+              </li>
+            </ul>
+
+            <h3 className="font-bold text-lg mb-2">How We Use Your Data:</h3>
+            <ul className="list-disc list-inside space-y-2">
+              <li>
+                To provide real-time, AI-driven health insights tailored to you.
+              </li>
+              <li>
+                To create analytics, insights, and information for insurance
+                companies.
+              </li>
+              <li>
+                Any identifiable data will not be shared with your employer.
+              </li>
+            </ul>
+
+            <h3 className="font-bold text-lg mb-2">
+              Data Security &amp; Confidentiality:
+            </h3>
+            <p>All data is stored securely on cloud servers.</p>
+
+            <h3 className="font-bold text-lg mb-2">Future Use of Data:</h3>
+            <ul className="list-disc list-inside space-y-2">
+              <li>
+                Data may be used for future research and further enhancement of
+                our AI models.
+              </li>
+              <li>
+                Analytics generated through your data may be shared with your
+                insurance company for risk assessment and product development.
+                Any decisions made based on such analytics are at the user’s
+                risk.
+              </li>
+            </ul>
+
+            <h2 className="mt-20 font-bold text-xl mb-2">
+              6. Risks &amp; Discomforts
+            </h2>
+
+            <h3 className="font-bold text-lg mb-2">Potential Risks:</h3>
+            <ul className="list-disc list-inside space-y-2">
+              <li>
+                The mobile app may use additional battery power and cellular
+                data.
+              </li>
+              <li>
+                This study is not intended to provide medical advice or
+                emergency health monitoring.
+              </li>
+            </ul>
+
+            <h3 className="font-bold text-lg mb-2">Discomforts:</h3>
+            <ul className="list-disc list-inside space-y-2">
+              <li>
+                Minimal discomfort may arise from charging and wearing the
+                health tracker, and syncing data daily.
+              </li>
+            </ul>
+
+            <h2 className="mt-20 font-bold text-xl mb-2">7. Benefits</h2>
+
+            <h3 className="font-bold text-lg mb-2">Personal Benefits:</h3>
+            <ul className="list-disc list-inside space-y-2">
+              <li>
+                Gain access to personalized, AI-driven health insights that may
+                help you manage your health better.
+              </li>
+            </ul>
+
+            <h3 className="font-bold text-lg mb-2">Broader Benefits:</h3>
+            <ul className="list-disc list-inside space-y-2">
+              <li>
+                Your participation will help improve tools that may benefit the
+                wider firefighting community.
+              </li>
+            </ul>
+
+            <h2 className="mt-20 font-bold text-xl mb-2">
+              8. Withdrawal &amp; Data Retention
+            </h2>
+            <ul className="list-disc list-inside space-y-2">
+              <li>
+                You may withdraw from the study at any time by uninstalling the
+                app or selecting the “Withdraw Participation” option in the app.
+              </li>
+              <li>
+                If you withdraw, no new data will be collected, but previously
+                de-identified, aggregate data may continue to be used for
+                research and analytics purposes.
+              </li>
+              <li>
+                Upon withdrawal, you may request the deletion of any remaining
+                personal data by contacting us at: contact@mybeatshealth.com.
+              </li>
+            </ul>
+
+            <h2 className="mt-20 font-bold text-xl mb-2">
+              9. Contact Information
+            </h2>
+            <ul className="list-disc list-inside space-y-2">
+              <li>
+                You may chat with our team through the app if you face any
+                issues.
+              </li>
+              <li>
+                For questions about your rights as a participant and concerns
+                regarding your data or about this pilot study, please contact us
+                at: contact@mybeatshealth.com
+              </li>
+            </ul>
+
+            <h2 className="mt-20 font-bold text-xl mb-2">
+              10. Agreement to Participate
+            </h2>
+            <p>
+              To participate in the pilot test, you will need to acknowledge
+              that:
+            </p>
+            <ul className="list-disc list-inside mt-4 space-y-2">
+              <li>You have read and understood this Consent Form.</li>
+              <li>You voluntarily agree to participate in the pilot study.</li>
+              <li>
+                You consent to the collection, processing, use, and sharing of
+                your health data as described above.
+              </li>
+              <li>
+                You understand that you can withdraw at any time without
+                penalty.
+              </li>
+            </ul>
+            <p className="mt-32 font-semibold">Download MyBeats</p>
+            <div className="flex h-[40px] space-x-10 mt-4">
+              <img src={AppleStoreBtn} />
+              <img src={GooglePlayBtn} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default ConsentFormPage;
---
+++ src/app/main/pilot/PilotPage.tsx
@@ -0,0 +1,210 @@
+import React, { useState } from "react";
+import Navbar from "../landing/components/navbar/Navbar";
+import Footer from "../landing/components/footer/Footer";
+import Text from "app/shared-components/texts/Text";
+import MyBeats from "../../../assets/MyBeats.png";
+import FadeAnimatedContainer from "app/shared-components/containers/FadeAnimatedContainer";
+import Step1 from "../../../assets/Step1.png";
+import Step2 from "../../../assets/Step 2.png";
+import Step3 from "../../../assets/Step 3.png";
+import Step4 from "../../../assets/Step4.png";
+import DetailedCard from "../landing/components/detailed/DetailedCard";
+import Button from "app/shared-components/buttons/Button";
+import { useNavigate } from "react-router";
+import News1 from "../../../assets/news1.png";
+import News2 from "../../../assets/news4.png";
+import News3 from "../../../assets/news3.png";
+import AppleStoreBtn from "../../../assets/download_app_store.svg";
+import GooglePlayBtn from "../../../assets/googleplay.png";
+
+const TABS = [
+  "Privacy Policy for Policyholders",
+  "Privacy Policy for Insurance Companies",
+];
+
+const ARTICLES = [
+  {
+    heading: "U.S. Firefighter Fatalities due to Sudden Cardiac Death",
+    content:
+      "During 1995-2004, almost half of the total number of firefighters who died while on duty, fell victim to sudden cardiac death, and close to half of those with documented prior medical…",
+    imgSrc: News1,
+    linkTo: "https://fire.engineering.nyu.edu/home/documents/NFPA.pdf",
+  },
+  {
+    heading: "Sudden Cardiac Events in the Fire Service",
+    content:
+      "Despite the risks of burns/smoke inhalation and other myriad physical dangers inherent in firefighting, sudden cardiac event is the leading cause of firefighter on-duty deaths…",
+    imgSrc: News2,
+    linkTo: "https://fire.engineering.nyu.edu/home/documents/Skidmore.pdf",
+  },
+  {
+    heading: "Preventing Firefighter Fatalities due to Heart Attacks",
+    content:
+      "Firefighters are dying on the job from preventable cardiovascular conditions. Sudden cardiac death represents the most common cause of a fire fighter fatality…",
+    imgSrc: News3,
+    linkTo: "https://fire.engineering.nyu.edu/home/documents/NIOSH.pdf",
+  },
+];
+
+const ImageCard = ({ imgUrl }) => {
+  return (
+    <div className="h-[300px] w-screen md:h-[400px] md:w-full overflow-hidden">
+      <img
+        src={imgUrl}
+        className="h-full w-full object-contain"
+        alt="Preview"
+      />
+    </div>
+  );
+};
+
+const PilotPage = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="bg-gray-200 overflow-x-hidden h-full flex flex-col justify-between">
+      <Navbar />
+      <div className="pb-[100px] overflow-auto flex-1 flex flex-col items-center justify-center gap-40">
+        <div className="px-40 md:w-[1000px] flex flex-col gap-20">
+          <div className="text-center">
+            <Text variant="h2">
+              AI-enabled Data Driven Analytics for Insurance Companies
+            </Text>
+          </div>
+          <FadeAnimatedContainer delay={1} direction="bottom">
+            {/* <ShowcaseCard scale="large" tiltDirection="center" /> */}
+            <video controls poster={MyBeats}>
+              <source
+                src="https://fire.engineering.nyu.edu/home/documents/MyBeats_Final.mp4"
+                type="video/mp4"
+              />
+            </video>
+          </FadeAnimatedContainer>
+        </div>
+
+        <div className="text-center md:w-[1200px] md:px-40 md:text-left">
+          <div className="text-center">
+            <Text variant="h2">
+              Participate in our pilot test in four easy steps
+            </Text>
+          </div>
+
+          <div className="flex flex-col md:flex-row md:space-x-32 md:space-y-0 space-y-32">
+            <div className="flex-1 mt-48">
+              <FadeAnimatedContainer direction="left">
+                <div className="h-ful flex flex-col justify-center space-y-20">
+                  <Text variant="h2">STEP 1: DOWNLOAD THE APP</Text>
+                  <Text variant="body2">
+                    Search for MyBeats in your mobile app store or click on
+                    buttons below.
+                  </Text>
+                </div>
+                <div className="flex h-[40px] space-x-10 mt-10 justify-center md:justify-start">
+                  <img src={AppleStoreBtn} />
+                  <img src={GooglePlayBtn} />
+                </div>
+              </FadeAnimatedContainer>
+            </div>
+
+            <div className="flex flex-1">
+              <FadeAnimatedContainer direction="right">
+                <ImageCard imgUrl={Step1} />
+              </FadeAnimatedContainer>
+            </div>
+          </div>
+
+          <div className="flex flex-col-reverse md:flex-row md:space-x-40 space-y-32">
+            <div className="flex flex-1 mt-48">
+              <FadeAnimatedContainer direction="left">
+                <ImageCard imgUrl={Step2} />
+              </FadeAnimatedContainer>
+            </div>
+            <div className="flex-1">
+              <FadeAnimatedContainer direction="right">
+                <div className="h-full flex flex-col justify-center space-y-20">
+                  <Text variant="h2">STEP 2: COMPLETE REGISTRATION</Text>
+                  <Text variant="body2">
+                    Login into the app and provide{" "}
+                    <a href="/consent">consent</a> to collect your health data
+                  </Text>
+                </div>
+              </FadeAnimatedContainer>
+            </div>
+          </div>
+
+          <div className="flex flex-col md:flex-row md:space-x-32 space-y-32">
+            <div className="flex-1 mt-48">
+              <FadeAnimatedContainer direction="left">
+                <div className="h-full flex flex-col justify-center space-y-20">
+                  <Text variant="h2">STEP 3: ADD YOUR HEALTH TRACKER</Text>
+                  <Text variant="body2">
+                    Allow us to access your wearables data by adding your health
+                    tracker
+                  </Text>
+                </div>
+              </FadeAnimatedContainer>
+            </div>
+
+            <div className="flex flex-1">
+              <FadeAnimatedContainer direction="right">
+                <ImageCard imgUrl={Step3} />
+              </FadeAnimatedContainer>
+            </div>
+          </div>
+
+          <div className="flex flex-col-reverse md:flex-row md:space-x-40 space-y-32">
+            <div className="flex flex-1 mt-48">
+              <FadeAnimatedContainer direction="left">
+                <ImageCard imgUrl={Step4} />
+              </FadeAnimatedContainer>
+            </div>
+            <div className="flex-1">
+              <FadeAnimatedContainer direction="right">
+                <div className="h-full flex flex-col justify-center space-y-20">
+                  <Text variant="h2">STEP 4: SYNC YOUR HEALTH TRACKER</Text>
+                  <Text variant="body2">
+                    Daily sync your health tracker to receive AI-enabled
+                    personalized health insights
+                  </Text>
+                </div>
+              </FadeAnimatedContainer>
+            </div>
+          </div>
+        </div>
+
+        {/* <div className="flex items-end justify-end">
+          <Button onClick={() => navigate("/consent")} variant="contained">
+            <span className="text-white">Read More</span>
+          </Button>
+        </div> */}
+
+        <div className="px-40 md:w-[1000px] mt-48">
+          <div className="text-center">
+            <Text variant="h2">Informational Articles</Text>
+          </div>
+
+          <div className="flex-col space-y-20 flex md:flex-row md:space-x-20 md:space-y-0 mt-48">
+            {ARTICLES.map((article) => (
+              <div
+                onClick={() =>
+                  window.open(article.linkTo, "_blank", "noopener,noreferrer")
+                }
+                className="cursor-pointer flex flex-col space-y-20"
+              >
+                <div className="flex flex-1 rounded-xl cursor-pointer">
+                  <img src={article.imgSrc} className="rounded-xl" />
+                </div>
+
+                <Text variant="body1">{article.heading}</Text>
+                <Text>{article.content}</Text>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default PilotPage;
